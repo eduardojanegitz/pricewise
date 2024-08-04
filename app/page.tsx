5563431dfc3b210a7e8ac9a33fd81bed3ac98087
Eduardo Alves <edu0alves00@gmail.com>
2024-08-04 12:28:46 -0300

    feat: Iniciando página inicial

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,44 @@
+import Image from "next/image";
+
 const Home = () => {
   return (
-    <div>Home</div>
-  )
-}
+    <>
+      <section className="px-6 md:px=20 py-24 border-2 border-red-500">
+        <div className="flex max-xl:flex-col gap-16">
+          <div className="flex flex-col justify-center">
+            <p className="small-text">
+              Compra inteligente começa aqui
+              <Image
+                src="/assets/icons/arrow-right.svg"
+                alt="arrow-right"
+                width={16}
+                height={16}
+              />
+            </p>
+            <h1 className="head-text">
+              Liberte o poder do <span className="text-primary">PriceWise</span>
+            </h1>
+            <p className="mt-6">
+              Análises poderosas e de autoatendimento de produtos e crescimento
+              para ajudá-lo a converter, engajar e reter mais.
+            </p>
+            SearchBar
+          </div>
+          HeroCarousel
+        </div>
+      </section>
 
-export default Home
+      <section className="trending-section">
+        <h2 className="section-text">Trending</h2>
+
+        <div className="flex flex-wrap gap-x-8 gap-y-16">
+          {["Apple Iphone 15", "Livro"].map((product) => (
+            <div>{product}</div>
+          ))}
+        </div>
+      </section>
+    </>
+  );
+};
+
+export default Home;
